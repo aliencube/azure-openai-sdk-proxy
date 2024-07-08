fdce7349cd8fd761b21b9d0d32151bef5ffe0c6c
Justin Yoo <justin.yoojh@gmail.com>
2024-07-08 16:15:58 +0900

    Remove unnecessary apps

--- src.bak.20240706/AzureOpenAIProxy.ApiApp/Attributes/OpenApiParameterIgnoreAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AzureOpenAIProxy.ApiApp.Attributes;
-
-/// <summary>
-/// This represents the attribute entity for parameters to be ignored from the OpenAPI document generation.
-/// </summary>
-[AttributeUsage(AttributeTargets.Parameter)]
-public class OpenApiParameterIgnoreAttribute : Attribute
-{
-}
