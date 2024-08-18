eaf7c930b432278e63bd52832116876a0b705f27
Justin Yoo <justin.yoojh@gmail.com>
2024-08-18 11:48:56 +0900

    Add WithKeyVault method (#228)

--- test/AzureOpenAIProxy.ApiApp.Tests/Extensions/ServiceCollectionExtensionsTests.cs
+++ test/AzureOpenAIProxy.ApiApp.Tests/Extensions/ServiceCollectionExtensionsTests.cs
@@ -83,14 +83,89 @@ public class ServiceCollectionExtensionsTests
     }
 
     [Theory]
-    [InlineData("http://localhost")]
-    public void Given_AppSettings_When_Invoked_AddKeyVaultService_Then_It_Should_Return_SecretClient(string vaultUri)
+    [InlineData(default(string), "secret-name")]
+    [InlineData("", "secret-name")]
+    public void Given_NullOrEmpty_VaultUri_When_Invoked_AddKeyVaultService_Then_It_Should_Throw_Exception(string? vaultUri, string secretName)
+    {
+        // Arrange
+        var services = new ServiceCollection();
+        var dict = new Dictionary<string, string>()
+        {
+            { "Azure:KeyVault:VaultUri", vaultUri! },
+            { "Azure:KeyVault:SecretName", secretName },
+        };
+#pragma warning disable CS8620 // Argument cannot be used for parameter due to differences in the nullability of reference types.
+        var config = new ConfigurationBuilder().AddInMemoryCollection(dict).Build();
+#pragma warning restore CS8620 // Argument cannot be used for parameter due to differences in the nullability of reference types.
+        services.AddSingleton<IConfiguration>(config);
+        services.AddKeyVaultService();
+
+        // Act
+        Action action = () => services.BuildServiceProvider().GetService<SecretClient>();
+
+        // Assert
+        action.Should().Throw<InvalidOperationException>();
+    }
+
+    [Theory]
+    [InlineData("http://localhost", default(string))]
+    [InlineData("http://localhost", "")]
+    public void Given_NullOrEmpty_SecretName_When_Invoked_AddKeyVaultService_Then_It_Should_Throw_Exception(string vaultUri, string? secretName)
     {
         // Arrange
         var services = new ServiceCollection();
         var dict = new Dictionary<string, string>()
         {
             { "Azure:KeyVault:VaultUri", vaultUri },
+            { "Azure:KeyVault:SecretName", secretName! },
+        };
+#pragma warning disable CS8620 // Argument cannot be used for parameter due to differences in the nullability of reference types.
+        var config = new ConfigurationBuilder().AddInMemoryCollection(dict).Build();
+#pragma warning restore CS8620 // Argument cannot be used for parameter due to differences in the nullability of reference types.
+        services.AddSingleton<IConfiguration>(config);
+        services.AddKeyVaultService();
+
+        // Act
+        Action action = () => services.BuildServiceProvider().GetService<SecretClient>();
+
+        // Assert
+        action.Should().Throw<InvalidOperationException>();
+    }
+
+    [Theory]
+    [InlineData("abcde", "secret-name")]
+    public void Given_Invalid_VaultUri_When_Invoked_AddKeyVaultService_Then_It_Should_Throw_Exception(string vaultUri, string secretName)
+    {
+        // Arrange
+        var services = new ServiceCollection();
+        var dict = new Dictionary<string, string>()
+        {
+            { "Azure:KeyVault:VaultUri", vaultUri },
+            { "Azure:KeyVault:SecretName", secretName },
+        };
+#pragma warning disable CS8620 // Argument cannot be used for parameter due to differences in the nullability of reference types.
+        var config = new ConfigurationBuilder().AddInMemoryCollection(dict).Build();
+#pragma warning restore CS8620 // Argument cannot be used for parameter due to differences in the nullability of reference types.
+        services.AddSingleton<IConfiguration>(config);
+        services.AddKeyVaultService();
+
+        // Act
+        Action action = () => services.BuildServiceProvider().GetService<SecretClient>();
+
+        // Assert
+        action.Should().Throw<UriFormatException>();
+    }
+
+    [Theory]
+    [InlineData("http://localhost", "secret-name")]
+    public void Given_AppSettings_When_Invoked_AddKeyVaultService_Then_It_Should_Return_SecretClient(string vaultUri, string secretName)
+    {
+        // Arrange
+        var services = new ServiceCollection();
+        var dict = new Dictionary<string, string>()
+        {
+            { "Azure:KeyVault:VaultUri", vaultUri },
+            { "Azure:KeyVault:SecretName", secretName },
         };
 #pragma warning disable CS8620 // Argument cannot be used for parameter due to differences in the nullability of reference types.
         var config = new ConfigurationBuilder().AddInMemoryCollection(dict).Build();
@@ -107,14 +182,15 @@ public class ServiceCollectionExtensionsTests
     }
 
     [Theory]
-    [InlineData("http://localhost")]
-    public void Given_AppSettings_When_Invoked_AddKeyVaultService_Then_It_Should_Return_VaultUri(string vaultUri)
+    [InlineData("http://localhost", "secret-name")]
+    public void Given_AppSettings_When_Invoked_AddKeyVaultService_Then_It_Should_Return_VaultUri(string vaultUri, string secretName)
     {
         // Arrange
         var services = new ServiceCollection();
         var dict = new Dictionary<string, string>()
         {
             { "Azure:KeyVault:VaultUri", vaultUri },
+            { "Azure:KeyVault:SecretName", secretName },
         };
 #pragma warning disable CS8620 // Argument cannot be used for parameter due to differences in the nullability of reference types.
         var config = new ConfigurationBuilder().AddInMemoryCollection(dict).Build();
