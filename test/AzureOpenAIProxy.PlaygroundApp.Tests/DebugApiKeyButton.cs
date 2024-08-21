12aa9c2d2a2d660a1706d8c238a54ab1842dcbaf
pmj-chosim <parkmin030505@naver.com>
2024-08-21 14:57:07 +0900

    apikeybutton

---
+++ test/AzureOpenAIProxy.PlaygroundApp.Tests/DebugApiKeyButton.cs
@@ -0,0 +1,45 @@
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace AzureOpenAIProxy.PlaygroundApp.Tests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class ApiKeyMaskingTests : PageTest
+{
+    [SetUp]
+    public async Task NavigateToHomePage(){
+        await Page.GotoAsync("https://localhost:5001/");
+        await Page.WaitForLoadStateAsync(LoadState.NetworkIdle); 
+    }
+
+    [Test]
+    public async Task DefaultMaskedState(){
+        var apiKeyInput = Page.Locator("input[name='ApiKey']");
+        var inputType = await apiKeyInput.GetAttributeAsync("type");
+        NUnit.Framework.Assert.AreEqual("password", inputType, "API 키 입력 필드는 기본적으로 마스킹되어 있어야 합니다.");
+    }
+
+    [Test]
+    public async Task ToggleMasking(){
+        var apiKeyInput = Page.Locator("input[name='ApiKey']");
+        var toggleMaskButton = Page.GetByRole(AriaRole.Button, new() { Name = "Mask" }); 
+        await Expect(apiKeyInput).ToHaveAttributeAsync("type", "password");
+        await toggleMaskButton.ClickAsync();
+        await Expect(apiKeyInput).ToHaveAttributeAsync("type", "text");
+
+        await toggleMaskButton.ClickAsync();
+        await Expect(apiKeyInput).ToHaveAttributeAsync("type", "password");
+    }
+
+    [Test]
+    [TestCase("test-api-key-1")]
+    [TestCase("example-key-123")]
+    public async Task DisplayApiKeyInput(string apiKey){
+        var apiKeyInput = Page.Locator("input[name='ApiKey']");
+
+        await apiKeyInput.FillAsync(apiKey);
+        await Expect(apiKeyInput).ToHaveValueAsync(apiKey);
+    }
+}
