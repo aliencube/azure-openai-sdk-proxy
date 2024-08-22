f1bb081cf3c7cbffe104da22622c4f9201acb2d6
Justin Yoo <justin.yoojh@gmail.com>
2024-08-22 19:05:54 +0900

    Add UI test for sidebar

---
+++ test/AzureOpenAIProxy.PlaygroundApp.Tests/Pages/HomePageTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Playwright.NUnit;
+
+namespace AzureOpenAIProxy.PlaygroundApp.Tests.Pages;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+[Property("Category", "Integration")]
+public class HomePageTests : PageTest
+{
+    [Test]
+    public async Task Given_Root_Page_When_Navigated_Then_It_Should_No_Sidebar()
+    {
+        // Arrange
+        await this.Page.GotoAsync("https://localhost:5001");
+
+        // Act
+        var sidebar = this.Page.Locator("div.sidebar");
+
+        // Assert
+        Expect(sidebar).Equals(null);
+    }
+}
