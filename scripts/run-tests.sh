c7100726eff14cd9448a57b419f988835edec201
Justin Yoo <justin.yoojh@gmail.com>
2024-08-23 00:04:56 +0900

    Add test scripts to run locally (#258)

---
+++ scripts/run-tests.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# Run unit tests and integration tests
+
+set -e
+
+# Function to show usage
+usage() {
+    echo "    This runs both unit tests and integration tests.
+
+    Usage: $0 [-c|--config|--configuration <Configuration>] [-h|--help]
+
+    Options:
+        -c|--config|--configuration     Configuration. Possible values are 'Debug' or 'Release'. Default is 'Debug'.
+        -h|--help                       Show this message.
+"
+    exit 0
+}
+
+# Default configuration
+CONFIGURATION="Debug"
+
+if [[ $# -eq 0 ]]; then
+    CONFIGURATION="Debug"
+fi
+
+while [[ "$1" != "" ]]; do
+    case $1 in
+        -c | --config | --configuration)
+            shift
+            CONFIGURATION="$1"
+        ;;
+
+        -h | --help)
+            usage
+            exit 0
+        ;;
+
+        *)
+            usage
+            exit 0
+        ;;
+    esac
+
+    shift
+done
+
+# Builds apps
+echo -e "\033[36mBuilding apps...\033[0m"
+
+dotnet restore
+dotnet build -c $CONFIGURATION
+
+# Runs unit tests
+echo -e "\033[36mInvoking unit tests...\033[0m"
+
+dotnet test ./test/AzureOpenAIProxy.AppHost.Tests -c $CONFIGURATION --no-build --logger "trx" --collect:"XPlat Code Coverage"
+dotnet test ./test/AzureOpenAIProxy.ApiApp.Tests -c $CONFIGURATION --no-build --logger "trx" --collect:"XPlat Code Coverage"
+
+# Runs integration tests
+echo -e "\033[36mInvoking integration tests...\033[0m"
+
+pwsh ./test/AzureOpenAIProxy.PlaygroundApp.Tests/bin/Debug/net8.0/playwright.ps1 install
+
+dotnet run --project ./src/AzureOpenAIProxy.AppHost --no-build &
+APP_PID=$!
+sleep 30
+
+dotnet test ./test/AzureOpenAIProxy.PlaygroundApp.Tests -c $CONFIGURATION --logger "trx" --collect:"XPlat Code Coverage"
+
+# Cleans up
+kill $APP_PID
