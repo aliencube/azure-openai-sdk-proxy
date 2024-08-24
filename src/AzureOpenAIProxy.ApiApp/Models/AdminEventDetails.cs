dc5ef5120ca099390c8f434b6c55452709be9d76
Capella87 <capella87@outlook.com>
2024-08-24 16:42:59 +0900

    Move less important properties from EventDetails to AdminEventDetails

--- src/AzureOpenAIProxy.ApiApp/Models/AdminEventDetails.cs
+++ src/AzureOpenAIProxy.ApiApp/Models/AdminEventDetails.cs
@@ -14,4 +14,39 @@ public class AdminEventDetails : EventDetails
     /// Gets or sets the event coorganizer email.
     /// </summary>
     public string? CoorganizerEmail { get; set; }
+
+    /// <summary>
+    /// Gets or sets the event description.
+    /// </summary>
+    public string? Description { get; set; }
+
+    /// <summary>
+    /// Gets or sets the event's start time.
+    /// </summary>
+    public required DateTimeOffset? DateStart { get; set; }
+
+    /// <summary>
+    /// Gets or sets the event's end time.
+    /// </summary>
+    public required DateTimeOffset? DateEnd { get; set; }
+
+    /// <summary>
+    /// Gets or sets the event's time zone.
+    /// </summary>
+    public required string? TimeZone { get; set; }
+
+    /// <summary>
+    /// Gets or sets the event's active status.
+    /// </summary>
+    public required bool? IsActive { get; set; }
+
+    /// <summary>
+    /// Gets or sets the organizer's name.
+    /// </summary>
+    public required string? OrganizerName { get; set; }
+
+    /// <summary>
+    /// Gets or sets the organizer's email address.
+    /// </summary>
+    public required string? OrganizerEmail { get; set; }
 }
--- src/AzureOpenAIProxy.ApiApp/Models/EventDetails.cs
+++ src/AzureOpenAIProxy.ApiApp/Models/EventDetails.cs
@@ -24,41 +24,6 @@ public class EventDetails
     public required string? Summary { get; set; }
 
     /// <summary>
-    /// Gets or sets the event description.
-    /// </summary>
-    public string? Description { get; set; }
-
-    /// <summary>
-    /// Gets or sets the event's start time.
-    /// </summary>
-    public required DateTimeOffset? DateStart { get; set; }
-
-    /// <summary>
-    /// Gets or sets the event's end time.
-    /// </summary>
-    public required DateTimeOffset? DateEnd { get; set; }
-
-    /// <summary>
-    /// Gets or sets the event's time zone.
-    /// </summary>
-    public required string? TimeZone { get; set; }
-
-    /// <summary>
-    /// Gets or sets the event's active status.
-    /// </summary>
-    public required bool? IsActive { get; set; }
-
-    /// <summary>
-    /// Gets or sets the organizer's name.
-    /// </summary>
-    public required string? OrganizerName { get; set; }
-
-    /// <summary>
-    /// Gets or sets the organizer's email address.
-    /// </summary>
-    public required string? OrganizerEmail { get; set; }
-
-    /// <summary>
     /// Gets or sets the event's maximum token capacity.
     /// </summary>
     public required int? MaxTokenCap { get; set; }
