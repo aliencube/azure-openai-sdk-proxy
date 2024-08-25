4a421511616d675327c133f9a86f25e475838a2b
Capella87 <capella87@outlook.com>
2024-08-25 15:08:44 +0900

    Add object, response tests in GetEventsOpenApiTests.cs

--- test/AzureOpenAIProxy.AppHost.Tests/ApiApp/Endpoints/GetEventsOpenApiTests.cs
+++ test/AzureOpenAIProxy.AppHost.Tests/ApiApp/Endpoints/GetEventsOpenApiTests.cs
@@ -15,12 +15,14 @@ public class GetEventsOpenApiTests(AspireAppHostFixture host) : IClassFixture<As
     {
         // Arrange
         using var httpClient = host.App!.CreateHttpClient("apiapp");
+
         // Act
         var json = await httpClient.GetStringAsync("/swagger/v1.0.0/swagger.json");
-        var openapi = JsonSerializer.Deserialize<JsonDocument>(json);
+        var apiDocument = JsonSerializer.Deserialize<JsonDocument>(json);
+
         // Assert
-        var result = openapi!.RootElement.GetProperty("paths")
-                                         .TryGetProperty("/events", out var property) ? property : default;
+        var result = apiDocument!.RootElement.GetProperty("paths")
+                                            .TryGetProperty("/events", out var property) ? property : default;
         result.ValueKind.Should().Be(JsonValueKind.Object);
     }
 
@@ -29,14 +31,15 @@ public class GetEventsOpenApiTests(AspireAppHostFixture host) : IClassFixture<As
     {
         // Arrange
         using var httpClient = host.App!.CreateHttpClient("apiapp");
+
         // Act
         var json = await httpClient.GetStringAsync("/swagger/v1.0.0/swagger.json");
-        var openapi = JsonSerializer.Deserialize<JsonDocument>(json);
+        var apiDocument = JsonSerializer.Deserialize<JsonDocument>(json);
 
         // Assert
-        var result = openapi!.RootElement.GetProperty("paths")
-                                        .GetProperty("/events")
-                                        .TryGetProperty("get", out var property) ? property : default;
+        var result = apiDocument!.RootElement.GetProperty("paths")
+                                            .GetProperty("/events")
+                                            .TryGetProperty("get", out var property) ? property : default;
         result.ValueKind.Should().Be(JsonValueKind.Object);
     }
 
@@ -49,13 +52,13 @@ public class GetEventsOpenApiTests(AspireAppHostFixture host) : IClassFixture<As
 
         // Act
         var json = await httpClient.GetStringAsync("/swagger/v1.0.0/swagger.json");
-        var openapi = JsonSerializer.Deserialize<JsonDocument>(json);
+        var apiDocument = JsonSerializer.Deserialize<JsonDocument>(json);
 
         // Assert
-        var result = openapi!.RootElement.GetProperty("paths")
-                                        .GetProperty("/events")
-                                        .GetProperty("get")
-                                        .TryGetProperty("tags", out var property) ? property : default;
+        var result = apiDocument!.RootElement.GetProperty("paths")
+                                            .GetProperty("/events")
+                                            .GetProperty("get")
+                                            .TryGetProperty("tags", out var property) ? property : default;
         result.ValueKind.Should().Be(JsonValueKind.Array);
         result.EnumerateArray().Select(p => p.GetString()).Should().Contain(tag);
     }
@@ -70,13 +73,54 @@ public class GetEventsOpenApiTests(AspireAppHostFixture host) : IClassFixture<As
 
         // Act
         var json = await httpClient.GetStringAsync("/swagger/v1.0.0/swagger.json");
-        var openapi = JsonSerializer.Deserialize<JsonDocument>(json);
+        var apiDocument = JsonSerializer.Deserialize<JsonDocument>(json);
 
-        // Assert =
-        var result = openapi!.RootElement.GetProperty("paths")
-                                        .GetProperty("/events")
-                                        .GetProperty("get")
-                                        .TryGetProperty(attribute, out var property) ? property : default;
+        // Assert
+        var result = apiDocument!.RootElement.GetProperty("paths")
+                                            .GetProperty("/events")
+                                            .GetProperty("get")
+                                            .TryGetProperty(attribute, out var property) ? property : default;
         result.ValueKind.Should().Be(JsonValueKind.String);
+    }
+
+    [Theory]
+    [InlineData("responses")]
+    public async Task Given_Resource_When_Invoked_Endpoint_Then_It_Should_Return_Object(string attribute)
+    {
+        // Arrange
+        using var httpClient = host.App!.CreateHttpClient("apiapp");
+
+        // Act
+        var json = await httpClient.GetStringAsync("/swagger/v1.0.0/swagger.json");
+        var apiDocument = JsonSerializer.Deserialize<JsonDocument>(json);
+
+        // Assert
+        var result = apiDocument!.RootElement.GetProperty("paths")
+                                            .GetProperty("/events")
+                                            .GetProperty("get")
+                                            .TryGetProperty(attribute, out var property) ? property : default;
+        result.ValueKind.Should().Be(JsonValueKind.Object);
+    }
+
+    [Theory]
+    [InlineData("200")]
+    [InlineData("401")]
+    [InlineData("500")]
+    public async Task Given_Resource_When_Invoked_Endpoint_Then_It_Should_Return_Response(string attribute)
+    {
+        // Arrange
+        using var httpClient = host.App!.CreateHttpClient("apiapp");
+
+        // Act
+        var json = await httpClient.GetStringAsync("/swagger/v1.0.0/swagger.json");
+        var apiDocument = JsonSerializer.Deserialize<JsonDocument>(json);
+
+        // Assert
+        var result = apiDocument!.RootElement.GetProperty("paths")
+                                            .GetProperty("/events")
+                                            .GetProperty("get")
+                                            .GetProperty("responses")
+                                            .TryGetProperty(attribute, out var property) ? property : default;
+        result.ValueKind.Should().Be(JsonValueKind.Object);
     }
 }
