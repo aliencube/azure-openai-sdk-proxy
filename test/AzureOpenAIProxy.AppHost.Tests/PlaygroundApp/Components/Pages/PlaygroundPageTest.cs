381fb20aaa3007c02ef537da3c4f71471dfb4c00
Lucy Oh <56earls@gmail.com>
2024-08-26 12:38:19 +0900

    Rename test method

--- test/AzureOpenAIProxy.AppHost.Tests/PlaygroundApp/Components/Pages/PlaygroundPageTest.cs
+++ test/AzureOpenAIProxy.AppHost.Tests/PlaygroundApp/Components/Pages/PlaygroundPageTest.cs
@@ -5,7 +5,7 @@ namespace AzureOpenAIProxy.AppHost.Tests.PlaygroundApp.Components.Pages;
 public class PlaygroundPageTest(AspireAppHostFixture host) : IClassFixture<AspireAppHostFixture>
 {
     [Fact]
-    public async Task GetPlaygroundEndpointReturnSuccess()
+    public async Task Given_Resource_When_Invoked_Endpoint_Then_It_Should_Return_OK()
     {
         // Arrange
         var client = host.App!.CreateHttpClient("playgroundapp");
