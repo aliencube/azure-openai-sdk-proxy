4a67022ef86137fbd91d7bb3f7c87d2245573060
yj_chun <yjchun626@gmail.com>
2024-08-27 15:00:33 +0900

    DeploymentModelList 컴포넌트 파일에 OnInitializedAsync(), Tests.razor에 OnAfterRenderAsync() 추가 및 수정 & 테스트 코드 수정

--- test/AzureOpenAIProxy.PlaygroundApp.Tests/UI/DeploymentModelListComponentTests.cs
+++ test/AzureOpenAIProxy.PlaygroundApp.Tests/UI/DeploymentModelListComponentTests.cs
@@ -27,13 +27,9 @@ namespace AzureOpenAIProxy.PlaygroundApp.Tests.UI
         {
             // given
             var componentLabel = Page.GetByText("Deployment");
-            await Expect(componentLabel).ToBeVisibleAsync();
             
-            // when
-            var expected = await Page.GetByText("Deployment").IsVisibleAsync();
-
-            // then
-            Assert.That(expected, Is.True);
+            // when & then
+            await Expect(componentLabel).ToBeVisibleAsync();
         }
 
         [Test]
@@ -41,48 +37,43 @@ namespace AzureOpenAIProxy.PlaygroundApp.Tests.UI
         public async Task Given_Dropdown_When_Waiting_Then_ShouldBeVisible()
         {
             // given
-            // var fluentSelect = Page.GetByRole(AriaRole.Combobox, new() { Name = "Deployment" });
             var fluentSelect = Page.Locator("fluent-select#deployment-model-list");
-            await Expect(fluentSelect).ToBeVisibleAsync();
 
-            // when
-            var expected = await fluentSelect.IsVisibleAsync();
-            
-            // then
-            Assert.That(expected, Is.True);
+            // when & then
+            await Expect(fluentSelect).ToBeVisibleAsync();
         }
 
         [Test]
         // 드롭다운의 옵션 값이 존재하는지 확인
-        public async Task Given_Dropdown_When_CountingOption_Then_ShouldOptionExist()
+        public async Task Given_Dropdown_When_ClickToShowOption_Then_ShouldOptionExist()
         {
             // given
             var fluentSelect = Page.Locator("fluent-select#deployment-model-list");
-            await Expect(fluentSelect).ToBeVisibleAsync();
-            await fluentSelect.ClickAsync();
 
             // when
-            int count = await fluentSelect.Locator("fluent-option").CountAsync();
-            //int count = await Page.QuerySelectorAllAsync("fluent-option").Count();
+            await fluentSelect.ClickAsync();
+            var fluentOptions = fluentSelect.Locator("fluent-option");
 
             // then
-            Assert.That(count, Is.GreaterThan(0));
+            for (int i = 0; i < await fluentOptions.CountAsync(); i++)
+            {
+                await Expect(fluentOptions.Nth(i)).ToBeVisibleAsync();
+            }
         }
 
         [Test]
         // 드롭다운의 옵션 값을 선택하면 부모 컴포넌트(페이지 컴포넌트)에 올바르게 업데이트 되는지 확인
-        public async Task Given_Dropdown_When_OptionSelected_Then_ShouldUpdateComponentValue()
+        public async Task Given_DropdownOption_When_OptionSelected_Then_ShouldUpdateComponentValue()
         {
             // given
             var fluentSelect = Page.Locator("fluent-select#deployment-model-list");
-            await Expect(fluentSelect).ToBeVisibleAsync();
             var expectedValue = "AZ"; // 컴포넌트 3번째 옵션 값
-            //var fluentOptions = fluentSelect.Locator("fluent-option");
-            await fluentSelect.ClickAsync(); // 드롭다운 클릭
-            await fluentSelect.Locator("fluent-option").Nth(2).ScrollIntoViewIfNeededAsync(); // 3번째 옵션 보이도록 스크롤
-            await fluentSelect.Locator("fluent-option").Nth(2).ClickAsync(); // 옵션 클릭
+            await fluentSelect.ClickAsync();
+            var fluentOptions = fluentSelect.Locator("fluent-option");
 
             // when
+            await fluentOptions.Nth(2).ScrollIntoViewIfNeededAsync(); // 3번째 옵션 보이도록 스크롤
+            await fluentOptions.Nth(2).ClickAsync(); // 옵션 클릭
             var actualValue = await Page.EvaluateAsync<string>("() => document.querySelector('fluent-select#deployment-model-list').value"); // 페이지 내 컴포넌트 값 가져오기
 
             // then
