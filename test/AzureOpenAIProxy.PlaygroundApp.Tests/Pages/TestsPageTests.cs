25e963097b3f8dababd208259b8e66d96c66b09f
o-ii <148066227+o-ii@users.noreply.github.com>
2024-08-31 22:21:51 +0900

    [Playground] Component - button to display a given input #171 (#236)

---
+++ test/AzureOpenAIProxy.PlaygroundApp.Tests/Pages/TestsPageTests.cs
@@ -0,0 +1,47 @@
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
+namespace AzureOpenAIProxy.PlaygroundApp.Tests.Pages;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+[Property("Category", "Integration")]
+public class TestsPageTests : PageTest
+{
+    public override BrowserNewContextOptions ContextOptions() => new()
+    {
+        IgnoreHTTPSErrors = true,
+    };
+
+    [SetUp]
+    public async Task Setup()
+    {
+        // Arrange
+        await Page.GotoAsync("https://localhost:5001/tests");
+        await Page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+    }
+
+    [Test]
+    public async Task Given_No_Input_When_DebugButton_Clicked_Then_Toast_Should_Show_NullMessage()
+    {
+        // Act
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Debug" }).ClickAsync();
+
+        // Assert
+        await Expect(Page.Locator(".fluent-toast-title")).ToHaveTextAsync("Input is null.");
+    }
+
+    [Test]
+    [TestCase(123, typeof(int))]
+    [TestCase(456, typeof(int))]
+    [TestCase(789, typeof(int))]
+    public async Task Given_Input_When_DebugButton_Clicked_Then_Toast_Should_Show_Input(int inputValue, Type inputType)
+    {
+        // Act
+        await Page.GetByRole(AriaRole.Radio, new() { Name = $"{inputValue}" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Debug" }).ClickAsync();
+
+        // Assert
+        await Expect(Page.Locator(".fluent-toast-title")).ToHaveTextAsync($"{inputValue} (Type: {inputType})");
+    }
+}
