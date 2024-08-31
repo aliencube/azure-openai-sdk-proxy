07a052cbeedb02d85fe26d421b7153e4c207db00
yj_chun <yjchun626@gmail.com>
2024-08-31 14:48:56 +0900

    Test 코드 - Arrange, Act, Assert 명시 및 파라미터라이징 등 수정, 드롭다운 컴포넌트 코드 - 필드 scope 수정

--- test/AzureOpenAIProxy.PlaygroundApp.Tests/UI/DeploymentModelListComponentTests.cs
+++ test/AzureOpenAIProxy.PlaygroundApp.Tests/UI/DeploymentModelListComponentTests.cs
@@ -1,6 +1,6 @@
 using Microsoft.Playwright.NUnit;
 using Microsoft.Playwright;
-using Assert = NUnit.Framework.Assert;
+using FluentAssertions;
 
 namespace AzureOpenAIProxy.PlaygroundApp.Tests.UI
 {
@@ -25,10 +25,10 @@ namespace AzureOpenAIProxy.PlaygroundApp.Tests.UI
         // 페이지에서 컴포넌트 레이블이 올바르게 표시되는지 확인
         public async Task Given_Label_When_Page_Loaded_Then_Label_Should_Be_Visible()
         {
-            // given
+            // Act
             var label = Page.GetByText("Deployment");
             
-            // when & then
+            // Assert
             await Expect(label).ToBeVisibleAsync();
         }
 
@@ -36,10 +36,10 @@ namespace AzureOpenAIProxy.PlaygroundApp.Tests.UI
         // 페이지에서 드롭다운 컴포넌트가 올바르게 표시되는지 확인
         public async Task Given_Dropdown_When_Page_Loaded_Then_Dropdown_Should_Be_Visible()
         {
-            // given
+            // Act
             var fluentSelect = Page.Locator("fluent-select#deployment-model-list");
 
-            // when & then
+            // Assert
             await Expect(fluentSelect).ToBeVisibleAsync();
         }
 
@@ -47,37 +47,41 @@ namespace AzureOpenAIProxy.PlaygroundApp.Tests.UI
         // 드롭다운의 옵션 값이 존재하는지 확인
         public async Task Given_Dropdown_When_Dropdown_Clicked_And_DropdownOptions_Appeared_Then_All_DropdownOptions_Should_Be_Visible()
         {
-            // given
+            // Arrange
             var fluentSelect = Page.Locator("fluent-select#deployment-model-list");
 
-            // when
+            // Act
             await fluentSelect.ClickAsync();
             var fluentOptions = fluentSelect.Locator("fluent-option");
 
-            // then
+            // Assert
             for (int i = 0; i < await fluentOptions.CountAsync(); i++)
             {
                 await Expect(fluentOptions.Nth(i)).ToBeVisibleAsync();
             }
         }
 
+        [TestCase("AZ", 2)]
+        [TestCase("CA", 4)]
+        [TestCase("CT", 6)]
+        [TestCase("FL", 8)]
         [Test]
         // 드롭다운의 옵션 값을 선택하면 부모 컴포넌트(페이지 컴포넌트)에 올바르게 업데이트 되는지 확인
-        public async Task Given_DropdownOptions_And_ExpectedValue_When_Third_DropdownOption_Selected_And_DropdownValue_Updated_Then_DropdownValue_Should_Match_ExpectedValue()
+        public async Task Given_DropdownOptions_And_ExpectedValue_When_Third_DropdownOption_Selected_And_DropdownValue_Updated_Then_DropdownValue_Should_Match_ExpectedValue(string exp, int n)
         {
-            // given
+            // Arrange
             var fluentSelect = Page.Locator("fluent-select#deployment-model-list");
             await fluentSelect.ClickAsync();
             var fluentOptions = fluentSelect.Locator("fluent-option");
-            var expectedValue = "AZ"; // 실제 컴포넌트 3번째 옵션 값
+            var expectedValue = exp; // 실제 컴포넌트 옵션 값
 
-            // when
-            await fluentOptions.Nth(2).ScrollIntoViewIfNeededAsync(); // 컴포넌트 3번째 옵션 보이도록 스크롤
-            await fluentOptions.Nth(2).ClickAsync(); // 옵션 클릭
+            // Act
+            await fluentOptions.Nth(n).ScrollIntoViewIfNeededAsync(); // 선택할 컴포넌트 옵션 보이도록 스크롤
+            await fluentOptions.Nth(n).ClickAsync(); // 옵션 클릭
             var actualValue = await Page.EvaluateAsync<string>("() => document.querySelector('fluent-select#deployment-model-list').value"); // 페이지 내 컴포넌트 값 가져오기
 
-            // then
-            Assert.That(actualValue, Is.EqualTo(expectedValue));
+            // Assert
+            actualValue.Should().Be(expectedValue);
         }
 
         [TearDown]
