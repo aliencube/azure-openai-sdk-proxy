dae27b38a290e7fbcefadfd94100560689f77b1d
Justin Yoo <justin.yoojh@gmail.com>
2024-09-01 12:53:06 +0900

    [Admin] Add admin event service layer (#286)

---
+++ src/AzureOpenAIProxy.ApiApp/Services/AdminEventService.cs
@@ -0,0 +1,85 @@
+﻿using AzureOpenAIProxy.ApiApp.Models;
+
+namespace AzureOpenAIProxy.ApiApp.Services;
+
+/// <summary>
+/// This provides interfaces to <see cref="AdminEventService"/> class.
+/// </summary>
+public interface IAdminEventService
+{
+    /// <summary>
+    /// Creates a new event.
+    /// </summary>
+    /// <param name="eventDetails">Event payload.</param>
+    /// <returns>Returns the event payload created.</returns>
+    Task<AdminEventDetails> CreateEvent(AdminEventDetails eventDetails);
+
+    /// <summary>
+    /// Gets the list of events.
+    /// </summary>
+    /// <returns>Returns the list of events.</returns>
+    Task<List<AdminEventDetails>> GetEvents();
+
+    /// <summary>
+    /// Gets the event details.
+    /// </summary>
+    /// <param name="eventId">Event ID.</param>
+    /// <returns>Returns the event details.</returns>
+    Task<AdminEventDetails> GetEvent(Guid eventId);
+
+    /// <summary>
+    /// Updates the event details.
+    /// </summary>
+    /// <param name="eventId">Event ID.</param>
+    /// <param name="eventDetails">Event details to update.</param>
+    /// <returns>Returns the updated event details.</returns>
+    Task<AdminEventDetails> UpdateEvent(Guid eventId, AdminEventDetails eventDetails);
+}
+
+/// <summary>
+/// This represents the service entity for admin event.
+/// </summary>
+public class AdminEventService : IAdminEventService
+{
+    /// <inheritdoc />
+    public async Task<AdminEventDetails> CreateEvent(AdminEventDetails eventDetails)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public async Task<List<AdminEventDetails>> GetEvents()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public async Task<AdminEventDetails> GetEvent(Guid eventId)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public async Task<AdminEventDetails> UpdateEvent(Guid eventId, AdminEventDetails eventDetails)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+/// <summary>
+/// This represents the extension class for <see cref="IServiceCollection"/>
+/// </summary>
+public static class AdminEventServiceExtensions
+{
+    /// <summary>
+    /// Adds the <see cref="AdminEventService"/> instance to the service collection.
+    /// </summary>
+    /// <param name="services"><see cref="IServiceCollection"/> instance.</param>
+    /// <returns>Returns <see cref="IServiceCollection"/> instance.</returns>
+    public static IServiceCollection AddAdminEventService(this IServiceCollection services)
+    {
+        services.AddScoped<IAdminEventService, AdminEventService>();
+
+        return services;
+    }
+}
