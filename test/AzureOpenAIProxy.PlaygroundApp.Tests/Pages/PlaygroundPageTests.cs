43ed06156995472310f945d7d0ce48386a9ac5fb
Justin Yoo <justin.yoojh@gmail.com>
2024-09-01 15:42:12 +0900

    [Playground] playground page layout (#291)

--- test/AzureOpenAIProxy.PlaygroundApp.Tests/Pages/PlaygroundPageTests.cs
+++ test/AzureOpenAIProxy.PlaygroundApp.Tests/Pages/PlaygroundPageTests.cs
@@ -1,3 +1,5 @@
+using FluentAssertions;
+
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
 
@@ -15,6 +17,34 @@ public class PlaygroundPageTests : PageTest
     {
         await Page.GotoAsync("https://localhost:5001/playground/");
         await Page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+    }
+
+    [Test]
+    public async Task Given_Page_When_Endpoint_Invoked_Then_It_Should_Show_Header()
+    {
+        // Arrange
+        var header = Page.Locator("div.layout")
+                         .Locator("header.header")
+                         .Locator("div.header-gutters")
+                         .Locator("h1");
+
+        // Act
+        var headerText = await header.TextContentAsync();
+
+        // Assert
+        headerText.Should().Be("Azure OpenAI Proxy Playground");
+    }
+
+    [Test]
+    [TestCase("config-grid")]
+    [TestCase("chat-grid")]
+    public async Task Given_Page_When_Endpoint_Invoked_Then_It_Should_Show_Panels(string id)
+    {
+        // Act
+        var panel = Page.Locator($"div.{id}");
+
+        // Assert
+        await Expect(panel).ToBeVisibleAsync();
     }
 
     [Test]
