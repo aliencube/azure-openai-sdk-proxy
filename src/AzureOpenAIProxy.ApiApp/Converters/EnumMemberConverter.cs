ab5712ce81d21b51092b61d616762bf9bcc3bc8c
jhmin99 <mjh012399@gmail.com>
2024-09-02 08:58:02 +0900

    Add EnumMemberConverter for custom Enum serialization

---
+++ src/AzureOpenAIProxy.ApiApp/Converters/EnumMemberConverter.cs
@@ -0,0 +1,66 @@
+using System.Runtime.Serialization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace AzureOpenAIProxy.ApiApp.Converters;
+
+public class EnumMemberConverter<T> : JsonConverter<T> where T : Enum
+{
+    public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var enumText = reader.GetString();
+
+        if (enumText == null)
+        {
+            throw new JsonException($"Unable to convert null to Enum \"{typeToConvert}\".");
+        }
+
+        foreach (var field in typeToConvert.GetFields())
+        {
+            var attribute = Attribute.GetCustomAttribute(field, typeof(EnumMemberAttribute)) as EnumMemberAttribute;
+
+            if (attribute != null && attribute.Value == enumText)
+            {
+                var value = field.GetValue(null);
+                if (value != null)
+                {
+                    return (T)value;
+                }
+            }
+            else if (field.Name == enumText)
+            {
+                var value = field.GetValue(null);
+                if (value != null)
+                {
+                    return (T)value;
+                }
+            }
+        }
+
+        throw new JsonException($"Unable to convert \"{enumText}\" to Enum \"{typeToConvert}\".");
+    }
+
+    public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+    {
+        var field = value.GetType().GetField(value.ToString());
+
+        if (field != null)
+        {
+            var attribute = Attribute.GetCustomAttribute(field, typeof(EnumMemberAttribute)) as EnumMemberAttribute;
+
+            if (attribute != null)
+            {
+                writer.WriteStringValue(attribute.Value);
+            }
+            else
+            {
+                writer.WriteStringValue(value.ToString());
+            }
+        }
+        else
+        {
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+
+}
