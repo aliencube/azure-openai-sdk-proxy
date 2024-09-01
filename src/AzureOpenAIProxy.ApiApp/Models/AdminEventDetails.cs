4c4ad4ca113fda24aded9d20d78745f6dea57507
Justin Yoo <justin.yoojh@gmail.com>
2024-09-01 12:34:52 +0900

    Update event payload

--- src/AzureOpenAIProxy.ApiApp/Models/AdminEventDetails.cs
+++ src/AzureOpenAIProxy.ApiApp/Models/AdminEventDetails.cs
@@ -1,4 +1,6 @@
-﻿namespace AzureOpenAIProxy.ApiApp.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace AzureOpenAIProxy.ApiApp.Models;
 
 /// <summary>
 /// This represent the event detail data for response by admin event endpoint.
@@ -13,31 +15,37 @@ public class AdminEventDetails : EventDetails
     /// <summary>
     /// Gets or sets the event start date.
     /// </summary>
+    [JsonRequired]
     public DateTimeOffset DateStart { get; set; }
 
     /// <summary>
     /// Gets or sets the event end date.
     /// </summary>
+    [JsonRequired]
     public DateTimeOffset DateEnd { get; set; }
 
     /// <summary>
     /// Gets or sets the event start to end date timezone.
     /// </summary>
+    [JsonRequired]
     public string TimeZone { get; set; } = string.Empty;
 
     /// <summary>
     /// Gets or sets the event active status.
     /// </summary>
+    [JsonRequired]
     public bool IsActive { get; set; }
 
     /// <summary>
     /// Gets or sets the event organizer name.
     /// </summary>
+    [JsonRequired]
     public string OrganizerName { get; set; } = string.Empty;
 
     /// <summary>
     /// Gets or sets the event organizer email.
     /// </summary>
+    [JsonRequired]
     public string OrganizerEmail { get; set; } = string.Empty;
 
     /// <summary>
