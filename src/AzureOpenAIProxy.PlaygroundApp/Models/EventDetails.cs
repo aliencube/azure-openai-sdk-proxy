7af315dec376a820474b04c33b6394aa458455a6
PC_KYJ <illulizer@gmail.com>
2024-09-04 00:38:15 +0900

    Update PlaygroundApp Model 최신화

--- src/AzureOpenAIProxy.PlaygroundApp/Models/EventDetails.cs
+++ src/AzureOpenAIProxy.PlaygroundApp/Models/EventDetails.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace AzureOpenAIProxy.PlaygroundApp.Models;
 
 /// <summary>
@@ -8,25 +10,30 @@ public class EventDetails
     /// <summary>
     /// Gets or sets the event id.
     /// </summary>
-    public required string? EventId { get; set; }
+    [JsonRequired]
+    public Guid EventId { get; set; }
 
     /// <summary>
     /// Gets or sets the event title name.
     /// </summary>
-    public required string? Title { get; set; }
+    [JsonRequired]
+    public string Title { get; set; } = string.Empty;
 
     /// <summary>
     /// Gets or sets the event summary.
     /// </summary>
-    public required string? Summary { get; set; }
+    [JsonRequired]
+    public string Summary { get; set; } = string.Empty;
 
     /// <summary>
     /// Gets or sets the Azure OpenAI Service request max token capacity.
     /// </summary>
-    public required int? MaxTokenCap { get; set; }
+    [JsonRequired]
+    public int MaxTokenCap { get; set; }
 
     /// <summary>
     /// Gets or sets the Azure OpenAI Service daily request capacity.
     /// </summary>
-    public required int? DailyRequestCap { get; set; }
+    [JsonRequired]
+    public int DailyRequestCap { get; set; }
 }
