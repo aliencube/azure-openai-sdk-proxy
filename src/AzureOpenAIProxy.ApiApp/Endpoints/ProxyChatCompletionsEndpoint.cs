ca09db1ff999d35f6dbd9e66ef7abc15d1c2b4ae
jhmin99 <mjh012399@gmail.com>
2024-09-04 10:16:31 +0900

    Removed `.Produces<ChatCompletion>` line from the `AddChatCompletions` endpoint.

--- src/AzureOpenAIProxy.ApiApp/Endpoints/ProxyChatCompletionsEndpoint.cs
+++ src/AzureOpenAIProxy.ApiApp/Endpoints/ProxyChatCompletionsEndpoint.cs
@@ -56,7 +56,6 @@ public static class ProxyChatCompletionsEndpoint
         // TODO: Check both request/response payloads
         .Accepts<ChatCompletionOptions>(contentType: "application/json")
         .Produces<CreateChatCompletionResponse>(statusCode: StatusCodes.Status200OK, contentType: "application/json")
-        //.Produces<ChatCompletion>(statusCode: StatusCodes.Status200OK, contentType: "application/json")
         // TODO: Check both request/response payloads
         .Produces(statusCode: StatusCodes.Status401Unauthorized)
         .Produces<string>(statusCode: StatusCodes.Status500InternalServerError, contentType: "text/plain")
