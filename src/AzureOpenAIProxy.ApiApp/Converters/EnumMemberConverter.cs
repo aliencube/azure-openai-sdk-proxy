ba014a94744ef88348e10698fc33013293828742
jhmin99 <mjh012399@gmail.com>
2024-09-06 16:30:10 +0900

    Add newline between using statements and class declaration in EnumMemberConverter.cs

--- src/AzureOpenAIProxy.ApiApp/Converters/EnumMemberConverter.cs
+++ src/AzureOpenAIProxy.ApiApp/Converters/EnumMemberConverter.cs
@@ -3,6 +3,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace AzureOpenAIProxy.ApiApp.Converters;
+
 /// <summary>
 /// This represents the converter entity for <see cref="EnumMemberAttribute"/>.
 /// </summary>
