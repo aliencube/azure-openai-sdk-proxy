a168b3caf0c5a4dc180872764117813b739d3b01
yj_chun <yjchun626@gmail.com>
2024-09-07 21:07:59 +0900

    SystemMessageTabComponentTests.cs 업데이트(버튼 및 textarea 테스트 추가)

--- test/AzureOpenAIProxy.PlaygroundApp.Tests/UI/SystemMessageTabComponentTests.cs
+++ test/AzureOpenAIProxy.PlaygroundApp.Tests/UI/SystemMessageTabComponentTests.cs
@@ -23,19 +23,7 @@ public class SystemMessageTabComponentTests : PageTest
         await Page.WaitForLoadStateAsync(LoadState.NetworkIdle);
     }
 
-    [Test]
-    [TestCase("system-message-tab-label", "System message")]
-    public async Task Given_Label_When_Page_Loaded_Then_Label_Should_Be_Visible(string id, string expected)
-    {
-        // Arrange
-        var label = Page.Locator($"label#{id}");
-
-        // Act
-        var result = await label.TextContentAsync();
-
-        // Assert
-        result.Should().Be(expected);
-    }
+    // label 보이는지 테스트(TestsPageTests.cs에 이미 있음)
     
     // 버튼 보이는지 테스트
     [Test]
@@ -50,11 +38,11 @@ public class SystemMessageTabComponentTests : PageTest
         await Expect(button).ToBeVisibleAsync();
     }
 
-    // 버튼 비활성화 여부 테스트(초기 화면)
+    // 버튼 비활성화 여부 테스트(초기 로드) - 'disabled' 속성이 있으면 비활성화된 상태
     [Test]
     [TestCase("system-message-tab-apply-button")]
     [TestCase("system-message-tab-reset-button")]
-    public async Task Given_Button_When_Page_Loaded_Then_Button_Should_Be_Disabled(string id)
+    public async Task Given_Button_When_Page_Loaded_Then_The_Button_Should_Be_Disabled(string id)
     {
         // Arrange
         var button = Page.Locator($"fluent-button#{id}");
@@ -66,7 +54,7 @@ public class SystemMessageTabComponentTests : PageTest
         isButtonDisabled.Should().NotBeNull();
     }
 
-    // 텍스트 박스 보이는지 테스트
+    // textarea 보이는지 테스트
     [Test]
     [TestCase("system-message-tab-textarea")]
     public async Task Given_TextArea_When_Page_Loaded_Then_TextArea_Should_Be_Visible(string id)
@@ -78,7 +66,7 @@ public class SystemMessageTabComponentTests : PageTest
         await Expect(textarea).ToBeVisibleAsync();
     }
 
-    // 텍스트 박스 default 값 테스트
+    // textarea 초기 로드 시 default 값으로 설정되어 있는지 테스트
     [Test]
     [TestCase("debug-button-system-message-tab", "You are an AI assistant that helps people find information.", typeof(string))]
     public async Task Given_TextArea_When_Page_Loaded_Then_TextArea_Should_Have_Default_Value(string buttonId, string expectedValue, Type expectedType)
@@ -88,73 +76,124 @@ public class SystemMessageTabComponentTests : PageTest
 
         // Act
         await debugButton.ClickAsync();
-        // var actualValue = await textarea.TextContentAsync();
 
         // Assert
         await Expect(Page.Locator(".fluent-toast-title")).ToHaveTextAsync($"{expectedValue} (Type: {expectedType})");
     }
 
-    // system message textarea 값 변경했을 때 apply, reset 버튼 활성화 여부 테스트
+    // textarea 값 변경되면 apply 버튼 & reset 버튼 활성화 되는지 테스트
+    /// -> 'disabled' 속성이 없으면 활성화된 상태
     [Test]
-    [TestCase("system-message-tab-apply-button")]
-    [TestCase("system-message-tab-reset-button")]
-    public async Task Given_TextArea_When_Value_Changed_Then_Buttons_Should_Be_Enabled(string id)
+    public async Task Given_ApplyButton_And_ResetButton_When_TextArea_Value_Changed_Then_All_Buttons_Should_Be_Enabled()
     {
         // Arrange
-        // var textarea = Page.Locator("fluent-text-area#system-message-tab-textarea");
-        var button = Page.Locator($"fluent-button#{id}");
+        var applyButton = Page.Locator($"fluent-button#system-message-tab-apply-button");
+        var resetButton = Page.Locator("fluent-button#system-message-tab-reset-button");
         var newValue = "New system message";
 
         // Act
-        await Page.EvaluateAsync($"document.querySelector('fluent-text-area#system-message-tab-textarea').value = '{newValue}'");
+        await Page.EvaluateAsync(@$"
+                const textarea = document.querySelector('fluent-text-area#system-message-tab-textarea');
+                textarea.value = '{newValue}';
+                textarea.dispatchEvent(new Event('input', {{ bubbles: true }}));
+        ");
+        var isApplyButtonDisabled = await applyButton.GetAttributeAsync("disabled");
+        var isResetButtonDisabled = await resetButton.GetAttributeAsync("disabled");
 
         // Assert
-        var isButtonDisabled = await button.GetAttributeAsync("disabled");
-        isButtonDisabled.Should().BeNullOrEmpty();
+        Assert.Multiple(() =>
+        {
+            isApplyButtonDisabled.Should().BeNull();
+            isResetButtonDisabled.Should().BeNull();
+        });
     }
 
-    // apply 버튼 작동 테스트(textarea 값 적용 뒤 apply 버튼만 비활성화)
+    // textarea 값이 기본값과 같은 경우 reset 버튼 비활성화 상태여야 함
+    /// -> 값 변경했다가 다시 default 값으로 그대로 작성하면 reset 버튼 비활성화 되는지 확인
     [Test]
-    [TestCase("New system message")]
-    public async Task Given_ApplyButton_When_Clicked_Then_Changes_Should_Be_Applied_And_Buttons_Updated(string message)
+    [TestCase("You are an AI assistant that helps people find information.")]
+    public async Task Given_ResetButton_When_Changed_TextArea_Value_Matches_Default_Then_ResetButton_Should_Be_Disabled(string defaultValue)
     {
         // Arrange
-        //var textarea = await Page.EvaluateAsync<IElementHandle>("document.querySelector('fluent-text-area#system-message-tab-textarea')");
+        var resetButton = Page.Locator($"fluent-button#system-message-tab-reset-button");
+        var newValue = "New system message";
+
+        // Act
+        await Page.EvaluateAsync(@$"
+                const textarea = document.querySelector('fluent-text-area#system-message-tab-textarea');
+                textarea.value = '{newValue}';
+                textarea.dispatchEvent(new Event('input', {{ bubbles: true }}));
+        ");
+        await Page.EvaluateAsync(@$"
+                const textarea = document.querySelector('fluent-text-area#system-message-tab-textarea');
+                textarea.value = '{defaultValue}';
+                textarea.dispatchEvent(new Event('input', {{ bubbles: true }}));
+        ");
+        var isResetButtonDisabled = await resetButton.GetAttributeAsync("disabled");
+
+        // Assert
+        isResetButtonDisabled.Should().NotBeNull();
+    }
+
+    // apply 버튼 작동 테스트(textarea 값 적용 & apply 버튼 다시 비활성화)
+    [Test]
+    [TestCase("1 New system message 1", typeof(string))]
+    [TestCase("2 New system message 2", typeof(string))]
+    [TestCase("3 New system message 3", typeof(string))]
+    public async Task Given_ApplyButton_When_Clicked_Then_Changed_TextArea_Value_Should_Be_Applied_And_ApplyButton_Should_Be_Disabled(string expectedValue, Type expectedType)
+    {
+        // Arrange
         var applyButton = Page.Locator("fluent-button#system-message-tab-apply-button");
         var debugButton = Page.Locator($"fluent-button#debug-button-system-message-tab");
-        var newValue = message;
 
         // Act
-        await Page.EvaluateAsync($"document.querySelector('fluent-text-area#system-message-tab-textarea').value = '{newValue}'");
+        await Page.EvaluateAsync(@$"
+                const textarea = document.querySelector('fluent-text-area#system-message-tab-textarea');
+                textarea.value = '{expectedValue}';
+                textarea.dispatchEvent(new Event('input', {{ bubbles: true }}));
+        ");
         await applyButton.ClickAsync();
         await debugButton.ClickAsync();
-        var isApplyButtonDisabled = await applyButton.IsDisabledAsync();
-        var actualValue = await Page.EvaluateAsync<string>("document.querySelector('fluent-text-area#system-message-tab-textarea').value");
+        var isApplyButtonDisabled = await applyButton.GetAttributeAsync("disabled");
 
         // Assert
-        //isApplyButtonDisabled.Should().BeTrue();
-        actualValue.Should().Be(newValue);
+        Assert.Multiple(async () =>
+        {
+            await Expect(Page.Locator(".fluent-toast-title")).ToHaveTextAsync($"{expectedValue} (Type: {expectedType})");
+            isApplyButtonDisabled.Should().NotBeNull();
+        });
     }
 
-    // reset 버튼 작동 테스트(textarea 값 초기화 뒤 apply, reset 버튼 모두 비활성화)
+    // reset 버튼 작동 테스트(textarea 값 초기화 & apply, reset 버튼 모두 비활성화)
     [Test]
-    public async Task Given_ResetButton_When_Clicked_Then_TextArea_Should_Have_Default_Value_And_Buttons_Updated()
+    [TestCase("You are an AI assistant that helps people find information.", typeof(string))]
+
+    public async Task Given_ResetButton_When_Clicked_Then_TextArea_Should_Have_Default_Value_And_All_Buttons_Should_Be_Disabled(string expectedValue, Type expectedType)
     {
         // Arrange
-        //var textarea = Page.Locator("fluent-text-area#system-message-tab-textarea");
         var applyButton = Page.Locator("fluent-button#system-message-tab-apply-button");
         var resetButton = Page.Locator("fluent-button#system-message-tab-reset-button");
+        var debugButton = Page.Locator($"fluent-button#debug-button-system-message-tab");
+        var newValue = "New system message";
 
         // Act
+        await Page.EvaluateAsync(@$"
+                const textarea = document.querySelector('fluent-text-area#system-message-tab-textarea');
+                textarea.value = '{newValue}';
+                textarea.dispatchEvent(new Event('input', {{ bubbles: true }}));
+        ");
         await resetButton.ClickAsync();
-        var actualValue = await Page.EvaluateAsync<string>("document.querySelector('fluent-text-area#system-message-tab-textarea').value");
+        await debugButton.ClickAsync();
+        var isApplyButtonDisabled = await applyButton.GetAttributeAsync("disabled");
+        var isResetButtonDisabled = await resetButton.GetAttributeAsync("disabled");
 
         // Assert
-        actualValue.Should().Be("You are an AI assistant that helps people find information.");
-        // var isApplyButtonDisabled = await applyButton.IsDisabledAsync();
-        // var isResetButtonDisabled = await resetButton.IsDisabledAsync();
-        // isApplyButtonDisabled.Should().BeTrue();
-        // isResetButtonDisabled.Should().BeTrue();
+        Assert.Multiple(async () =>
+        {
+            await Expect(Page.Locator(".fluent-toast-title")).ToHaveTextAsync($"{expectedValue} (Type: {expectedType})");
+            isApplyButtonDisabled.Should().NotBeNull();
+            isResetButtonDisabled.Should().NotBeNull();
+        });
     }
 
     [TearDown]
