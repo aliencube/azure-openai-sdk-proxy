4f2befcba933c8f5c3f33fba00d3b27768559ea4
jhmoon <sugaryfood@naver.com>
2024-09-07 14:41:31 +0900

    Fix DeploymentModelDetails class to have only Name field

--- src/AzureOpenAIProxy.ApiApp/Models/DeploymentModelDetails.cs
+++ src/AzureOpenAIProxy.ApiApp/Models/DeploymentModelDetails.cs
@@ -6,18 +6,9 @@ using System.Text.Json.Serialization;
 public class DeploymentModelDetails
 {
     /// <summary>
-    /// Gets or sets the deployment id of the model.
-    /// </summary>
-    [JsonRequired]
-    public int? Id { get; set; }
-
-    /// <summary>
     /// Gets or sets the deployment model name.
     /// </summary>
-    public string? Name { get; set; }
+    [JsonRequired]
+    public string Name { get; set; } = string.Empty;
 
-    /// <summary>
-    /// Gets or sets the version of deployment model.
-    /// </summary>
-    public string? Version { get; set; }
 }
