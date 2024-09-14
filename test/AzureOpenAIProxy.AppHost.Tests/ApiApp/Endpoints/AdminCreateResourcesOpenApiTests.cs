5fd1c8ce5439985359fcd751fac48f0c95799a29
macbook <rmk.ewha@ewhain.net>
2024-09-14 11:12:27 +0900

    Added tests for AdminResourceDetails and updated admin tag description

--- test/AzureOpenAIProxy.AppHost.Tests/ApiApp/Endpoints/AdminCreateResourcesOpenApiTests.cs
+++ test/AzureOpenAIProxy.AppHost.Tests/ApiApp/Endpoints/AdminCreateResourcesOpenApiTests.cs
@@ -4,6 +4,8 @@ using AzureOpenAIProxy.AppHost.Tests.Fixtures;
 
 using FluentAssertions;
 
+using IdentityModel.Client;
+
 namespace AzureOpenAIProxy.AppHost.Tests.ApiApp.Endpoints;
 
 public class AdminCreateResourcesOpenApiTests(AspireAppHostFixture host) : IClassFixture<AspireAppHostFixture>
@@ -39,7 +41,7 @@ public class AdminCreateResourcesOpenApiTests(AspireAppHostFixture host) : IClas
         // Assert
         var result = openapi!.RootElement.GetProperty("paths")
                                          .GetProperty("/admin/resources")
-                                         .TryGetProperty("post", out var property) ? property : default;
+                                         .GetProperty("post");
         result.ValueKind.Should().Be(JsonValueKind.Object);
     }
 
@@ -59,7 +61,7 @@ public class AdminCreateResourcesOpenApiTests(AspireAppHostFixture host) : IClas
         var result = openapi!.RootElement.GetProperty("paths")
                                          .GetProperty("/admin/resources")
                                          .GetProperty("post")
-                                         .TryGetProperty("tags", out var property) ? property : default;
+                                         .GetProperty("tags");
         result.ValueKind.Should().Be(JsonValueKind.Array);
         result.EnumerateArray().Select(p => p.GetString()).Should().Contain(tag);
     }
@@ -82,7 +84,7 @@ public class AdminCreateResourcesOpenApiTests(AspireAppHostFixture host) : IClas
         var result = openapi!.RootElement.GetProperty("paths")
                                          .GetProperty("/admin/resources")
                                          .GetProperty("post")
-                                         .TryGetProperty(attribute, out var property) ? property : default;
+                                         .GetProperty(attribute);
         result.ValueKind.Should().Be(JsonValueKind.String);
     }
 
@@ -103,7 +105,7 @@ public class AdminCreateResourcesOpenApiTests(AspireAppHostFixture host) : IClas
         var result = openapi!.RootElement.GetProperty("paths")
                                          .GetProperty("/admin/resources")
                                          .GetProperty("post")
-                                         .TryGetProperty(attribute, out var property) ? property : default;
+                                         .GetProperty(attribute);
         result.ValueKind.Should().Be(JsonValueKind.Object);
     }
 
@@ -127,7 +129,7 @@ public class AdminCreateResourcesOpenApiTests(AspireAppHostFixture host) : IClas
                                          .GetProperty("/admin/resources")
                                          .GetProperty("post")
                                          .GetProperty("responses")
-                                         .TryGetProperty(attribute, out var property) ? property : default;
+                                         .GetProperty(attribute);
         result.ValueKind.Should().Be(JsonValueKind.Object);
     }
 
@@ -144,7 +146,7 @@ public class AdminCreateResourcesOpenApiTests(AspireAppHostFixture host) : IClas
 
         // Assert
         var result = openapi!.RootElement.GetProperty("components")
-                                         .TryGetProperty("schemas", out var property) ? property : default;
+                                         .GetProperty("schemas");
         result.ValueKind.Should().Be(JsonValueKind.Object);
     }
 
@@ -162,7 +164,130 @@ public class AdminCreateResourcesOpenApiTests(AspireAppHostFixture host) : IClas
         // Assert
         var result = openapi!.RootElement.GetProperty("components")
                                          .GetProperty("schemas")
-                                         .TryGetProperty("AdminResourceDetails", out var property) ? property : default;
+                                         .GetProperty("AdminResourceDetails");
         result.ValueKind.Should().Be(JsonValueKind.Object);
+    }
+    
+    public static IEnumerable<object[]> AttributeData =>
+        [
+            ["resourceId", true, "string"],
+            ["friendlyName", true, "string"],
+            ["deploymentName", true, "string"],
+            ["resourceType", true, "string"],
+            ["endpoint", true, "string"],
+            ["apiKey", true, "string"],
+            ["region", true, "string"],
+            ["isActive", true, "boolean"]
+        ];
+
+    [Theory]
+    [MemberData(nameof(AttributeData))]
+    public async Task Given_Resource_When_Invoked_Endpoint_Then_It_Should_Return_Required(string attribute, bool isRequired, string type)
+    {
+        // Arrange
+        using var httpClient = host.App!.CreateHttpClient("apiapp");
+        await host.ResourceNotificationService.WaitForResourceAsync("apiapp", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
+
+        // Act
+        var json = await httpClient.GetStringAsync("/swagger/v1.0.0/swagger.json");
+        var openapi = JsonSerializer.Deserialize<JsonDocument>(json);
+
+        // Assert
+        var result = openapi!.RootElement.GetProperty("components")
+                                         .GetProperty("schemas")
+                                         .GetProperty("AdminResourceDetails")
+                                         .TryGetStringArray("required")
+                                         .ToList();
+        result.Contains(attribute).Should().Be(isRequired);
+    }
+
+    [Theory]
+    [MemberData(nameof(AttributeData))]
+    public async Task Given_Resource_When_Invoked_Endpoint_Then_It_Should_Return_Property(string attribute, bool isRequired, string type)
+    {
+        // Arrange
+        using var httpClient = host.App!.CreateHttpClient("apiapp");
+        await host.ResourceNotificationService.WaitForResourceAsync("apiapp", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
+
+        // Act
+        var json = await httpClient.GetStringAsync("/swagger/v1.0.0/swagger.json");
+        var openapi = JsonSerializer.Deserialize<JsonDocument>(json);
+
+        // Assert
+        var result = openapi!.RootElement.GetProperty("components")
+                                         .GetProperty("schemas")
+                                         .GetProperty("AdminResourceDetails")
+                                         .GetProperty("properties")
+                                         .GetProperty(attribute);
+        result.ValueKind.Should().Be(JsonValueKind.Object);
+    }
+
+    [Theory]
+    [MemberData(nameof(AttributeData))]
+    public async Task Given_Resource_When_Invoked_Endpoint_Then_It_Should_Return_Type(string attribute, bool isRequired, string type)
+    {
+        // Arrange
+        using var httpClient = host.App!.CreateHttpClient("apiapp");
+        await host.ResourceNotificationService.WaitForResourceAsync("apiapp", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
+
+        // Act
+        var json = await httpClient.GetStringAsync("/swagger/v1.0.0/swagger.json");
+        var openapi = JsonSerializer.Deserialize<JsonDocument>(json);
+
+        // Assert
+        var result = openapi!.RootElement.GetProperty("components")
+                                         .GetProperty("schemas")
+                                         .GetProperty("AdminResourceDetails")
+                                         .GetProperty("properties")
+                                         .GetProperty(attribute);
+
+        if (!result.TryGetProperty("type", out var typeProperty))
+        {
+            var refPath = result.TryGetString("$ref").TrimStart('#', '/').Split('/');
+            var refSchema = openapi.RootElement;
+
+            foreach (var part in refPath)
+            {
+                refSchema = refSchema.GetProperty(part);
+            }
+
+            typeProperty = refSchema.GetProperty("type");
+        }
+        
+        typeProperty.GetString().Should().Be(type);
+    }
+
+    [Fact]
+    public async Task Given_Resource_When_Invoked_Endpoint_Then_It_Should_Validate_ResourceType_As_Enum()
+    {
+        // Arrange
+        using var httpClient = host.App!.CreateHttpClient("apiapp");
+        await host.ResourceNotificationService.WaitForResourceAsync("apiapp", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
+
+        // Act
+        var json = await httpClient.GetStringAsync("/swagger/v1.0.0/swagger.json");
+        var openapi = JsonSerializer.Deserialize<JsonDocument>(json);
+
+        // Assert
+        var result = openapi!.RootElement.GetProperty("components")
+                                         .GetProperty("schemas")
+                                         .GetProperty("AdminResourceDetails")
+                                         .GetProperty("properties")
+                                         .GetProperty("resourceType");
+
+        var refPath = result.TryGetString("$ref").TrimStart('#', '/').Split('/');
+        var refSchema = openapi.RootElement;
+
+        foreach (var part in refPath)
+        {
+            refSchema = refSchema.GetProperty(part);
+        }
+
+        var enumValues = refSchema.GetProperty("enum")
+                                  .EnumerateArray()
+                                  .Select(p => p.GetString())
+                                  .ToList();
+
+        enumValues.Should().BeEquivalentTo(["none", "chat", "image"]);
     }
 }
