c636124d428a323147acabf1db553b8fc1b52ab9
PC_KYJ <illulizer@gmail.com>
2024-10-02 01:14:11 +0900

    Test: Add init timezone test

--- test/AzureOpenAIProxy.PlaygroundApp.Tests/Pages/NewEventDetailsPageTests.cs
+++ test/AzureOpenAIProxy.PlaygroundApp.Tests/Pages/NewEventDetailsPageTests.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using System;
+
+using FluentAssertions;
 
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
@@ -62,6 +64,65 @@ public class NewEventDetailsPageTests : PageTest
         inputDailyRequestCap.Should().NotBeNull();
         buttonAdd.Should().NotBeNull();
         buttonCancel.Should().NotBeNull();
+    }
+
+    [Test]
+    public async Task Given_Input_Event_Timezone_When_Initialized_Timezone_Then_It_Should_Update_Value()
+    {
+        // Arrange
+        await Page.WaitForSelectorAsync("#event-timezone");
+
+        string timeZone = await Page.EvaluateAsync<string>(@"() => Intl.DateTimeFormat().resolvedOptions().timeZone");
+
+        // Act
+        string inputTimezoneValue = await Page.GetAttributeAsync("#event-timezone", "current-value");
+
+        // Assert
+        inputTimezoneValue.Should().Be(timeZone);
+    }
+
+    [Test]
+    public async Task Given_Input_Event_Start_DateTime_When_Initialized_Timezone_Then_It_Should_Update_Value()
+    {
+        // Arrange
+        await Page.WaitForSelectorAsync("#event-start-date");
+        await Page.WaitForSelectorAsync("#event-start-time");
+
+        string timezoneId = await Page.EvaluateAsync<string>(@"() => Intl.DateTimeFormat().resolvedOptions().timeZone");
+        var timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(timezoneId);
+        DateTimeOffset currentTime = TimeZoneInfo.ConvertTime(DateTime.Now, timeZoneInfo);
+        var startTime = currentTime.AddHours(1).AddMinutes(-currentTime.Minute);
+
+
+        // Act
+        var inputStartDateValue = await Page.GetAttributeAsync("#event-start-date", "current-value");
+        var inputStartTimeValue = await Page.GetAttributeAsync("#event-start-time", "current-value");
+
+        // Assert
+        inputStartDateValue.Should().Be(startTime.ToString("yyyy-MM-dd"));
+        inputStartTimeValue.Should().Be(startTime.ToString("HH:mm"));
+    }
+
+    [Test]
+    public async Task Given_Input_Event_End_DateTime_When_Initialized_Timezone_Then_It_Should_Update_Value()
+    {
+        // Arrange
+        await Page.WaitForSelectorAsync("#event-end-date");
+        await Page.WaitForSelectorAsync("#event-end-time");
+
+        string timezoneId = await Page.EvaluateAsync<string>(@"() => Intl.DateTimeFormat().resolvedOptions().timeZone");
+        var timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(timezoneId);
+        DateTimeOffset currentTime = TimeZoneInfo.ConvertTime(DateTime.Now, timeZoneInfo);
+        var endTime = currentTime.AddDays(1).AddHours(1).AddMinutes(-currentTime.Minute);
+
+
+        // Act
+        var inputEndDateValue = await Page.GetAttributeAsync("#event-end-date", "current-value");
+        var inputEndTimeValue = await Page.GetAttributeAsync("#event-end-time", "current-value");
+
+        // Assert
+        inputEndDateValue.Should().Be(endTime.ToString("yyyy-MM-dd"));
+        inputEndTimeValue.Should().Be(endTime.ToString("HH:mm"));
     }
 
     [TearDown]
