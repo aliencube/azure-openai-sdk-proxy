a72bad9b214fa35833f7e0301967967ce9f107ca
juhangil <tensil94@gmail.com>
2024-10-16 04:58:23 +0900

    Add ChatParameters and adjust event call

---
+++ src/AzureOpenAIProxy.PlaygroundApp/Models/ChatParameters.cs
@@ -0,0 +1,14 @@
+﻿namespace AzureOpenAIProxy.PlaygroundApp.Models;
+
+public class ChatParameters
+{
+    public float pastMessages;
+    public float maxResponse;
+    public float temperature;
+    public float topP;
+
+    public IEnumerable<string>? stopSequence;
+
+    public float frequencyPenalty;
+    public float presencePenalty;
+}
